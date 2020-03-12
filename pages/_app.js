7441aaafb49a9a2dca9e840e0676511a8b387306
brianqian <qian.brian@gmail.com>
2020-03-12 02:51:57 -0400

    updated packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import App, { Container } from "next/app";
-import React from "react";
-import { ThemeProvider, createGlobalStyle } from "styled-components";
-import theme from "../src/data/cssTheme";
+import App from 'next/app';
+import React from 'react';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import theme from '../src/data/cssTheme';
 
 const GlobalStyle = createGlobalStyle`
 @import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,900&display=swap");
 body, html{
   margin: 0;
-  font-family: ${props => props.theme.textFont};
+  font-family: ${(props) => props.theme.textFont};
   max-width: 100vw;
 }
 *{
@@ -30,14 +30,12 @@ export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Container>
-        <ThemeProvider theme={theme}>
-          <>
-            <GlobalStyle />
-            <Component {...pageProps} />
-          </>
-        </ThemeProvider>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </>
+      </ThemeProvider>
     );
   }
 }
